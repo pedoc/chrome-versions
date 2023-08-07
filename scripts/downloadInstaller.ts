2258f747fde8151d1b1c3104d90a8bb160c7edc5
pedoc <pedoc@qq.com>
2023-08-07 17:36:24 +0800

    ignore download error

--- scripts/downloadInstaller.ts
+++ scripts/downloadInstaller.ts
@@ -14,24 +14,30 @@ export async function downloadInstaller(
 
   console.log('Downloading Chrome@%s on %s at url: %s', chromeVersion, chromeOs, url, headers);
 
-  const response = await Axios.get(url, {
-    responseType: 'stream',
-    maxRedirects: 5,
-    method: 'GET',
-    headers,
-  });
+  try {
+    const response = await Axios.get(url, {
+      responseType: 'stream',
+      maxRedirects: 5,
+      method: 'GET',
+      headers,
+    });
 
-  if (response.status === 404) {
-    //throw new Error('Not found');
+    if (response.status === 404) {
+      //throw new Error('Not found');
+      return '';
+    }
+
+    const file = Fs.createWriteStream(destinationPath);
+    response.data.pipe(file);
+    await new Promise((resolve, reject) => {
+      file.on('finish', resolve);
+      file.on('error', reject);
+    });
+    console.log('Finished download at %s', destinationPath);
+    return destinationPath;
+  }
+  catch (ex){
+    console.log('Error downloading Chrome@%s on %s at url: %s', chromeVersion, chromeOs, url, ex);
     return '';
   }
-
-  const file = Fs.createWriteStream(destinationPath);
-  response.data.pipe(file);
-  await new Promise((resolve, reject) => {
-    file.on('finish', resolve);
-    file.on('error', reject);
-  });
-  console.log('Finished download at %s', destinationPath);
-  return destinationPath;
 }
